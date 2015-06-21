32f7c16fbc4340e3172345e48200f7641d13203c
patowen95 <patowen95@gmail.com>
2015-06-21 14:04:03 -0700

    Edit copyright statement

--- screenpoint.h
+++ screenpoint.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 <copyright holder> <email>
+ * Copyright 2015 Patrick Owen patowen95@gmail.com
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
